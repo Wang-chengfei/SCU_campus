ba06d90e83afe92eac8fce15783297b4123333d3
Wang-chengfei <2677373210@qq.com>
2020-11-12 10:45:57 +0800

    2nd

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 5000
+    },
+    speed: 800,
+    loop: true
+});
