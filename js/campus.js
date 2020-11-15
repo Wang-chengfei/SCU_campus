29caa01bca8a2b4dfb9c247f400339f8f1caad76
Wang-chengfei <2677373210@qq.com>
2020-11-15 18:37:37 +0800

    11.15

--- js/campus.js
+++ js/campus.js
@@ -1,35 +1,32 @@
-var a=0;
-var c=document.querySelector(".big");
-var Bcom=document.querySelectorAll(".com1");
-var Scom=document.querySelectorAll(".com2");
-var belowimg=document.querySelector(".belowimg");
+var a = 0;
+var c = document.querySelector(".big");
+var Bcom = document.querySelectorAll(".com1");
+var Scom = document.querySelectorAll(".com2");
+var belowimg = document.querySelector(".belowimg");
 
 document.addEventListener("scroll", function () {
-for(var i=0;i<Bcom.length;i++){
-    if (window.pageYOffset >= c.offsetTop-600+i*650) {
-      a=1;
-      if(i%2==1){
-        Bcom[i].className="slide1-left";
-        Scom[i].className="slide2-left";
-      }else{
-        Bcom[i].className="slide1-right";
-        Scom[i].className="slide2-right";
+  for (var i = 0; i < Bcom.length; i++) {
+    if (window.pageYOffset >= c.offsetTop - 600 + i * 650) {
+      a = 1;
+      if (i % 2 == 1) {
+        Bcom[i].className = "slide1-left";
+        Scom[i].className = "slide2-left";
+      } else {
+        Bcom[i].className = "slide1-right";
+        Scom[i].className = "slide2-right";
       }
-    } 
-    
-    
-    if (window.pageYOffset < c.offsetTop-600+i*650 && window.pageYOffset>=c.offsetTop-1050+i*650 && a>0) {
-      if(i%2==1){
-        Bcom[i].className="slide1-rightenter";
-      Scom[i].className="slide2-rightenter";
-      }else{
-        Bcom[i].className="slide1-leftenter";
-        Scom[i].className="slide2-leftenter";
+    }
+
+
+    if (window.pageYOffset < c.offsetTop - 600 + i * 650 && window.pageYOffset >= c.offsetTop - 1050 + i * 650 && a > 0) {
+      if (i % 2 == 1) {
+        Bcom[i].className = "slide1-rightenter";
+        Scom[i].className = "slide2-rightenter";
+      } else {
+        Bcom[i].className = "slide1-leftenter";
+        Scom[i].className = "slide2-leftenter";
       }
-    }       
+    }
   }
 })
 
-belowimg.addEventListener("click",function(){
-    location.href="./index.html"
-})
--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,40 @@
-var nav=document.querySelector("#nav1");
-var rnav=document.querySelector("#nav2");
-var c=document.querySelector(".big");
-var b=0;
+var nav = document.querySelector("#nav1");
+var rnav = document.querySelector("#nav2");
+var c = document.querySelector(".big");
+var b = 0;
 
 document.addEventListener("scroll", function () {
 
-        if (window.pageYOffset >= c.offsetTop) {
-            rnav.className="rnav-out";
-            nav.className="nav-enter";
-            b=1;
-          }else{
-              if(b>0){
-                    rnav.className="rnav-enter";
-                    nav.className="nav-out";
-                }
-          }
-})       
+    if (window.pageYOffset > 0) {
+        rnav.className = "rnav-out";
+        nav.className = "nav-enter";
+        b = 1;
+    } else {
+        if (b > 0) {
+            rnav.className = "rnav-enter";
+            nav.className = "nav-out";
+        }
+    }
+})
+
+var bigBox = document.querySelector(".bigBox")
+function scoll(e) {
+    var temp = bigBox.className
+    var top = window.pageYOffset
+    if (top > 0) {
+        bigBox.className = "bigBox up"
+    }
+    if (top == 0) {
+        bigBox.className = "bigBox dowm"
+    }
+    if (temp != bigBox.className) {
+        document.body.style.overflowY = 'hidden'
+        setTimeout("note()", 800)
+    }
+}
+
+function note() {
+    document.body.style.overflowY = 'visible'
+}
+
+document.addEventListener('scroll', scoll)
